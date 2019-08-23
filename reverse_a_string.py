72bc7dffaa3a1f38fd16088c44c9d8641c7c0aa8
Aileen Liu <aileenliu184@gmail.com>
2019-08-22 18:20:23 -0700

    solved the reversed a string problem

---
+++ reverse_a_string.py
@@ -0,0 +1,10 @@
+def rev_string(astring):
+    """Return reverse of string.
+
+    You may NOT use the reversed() function!
+    """
+    return astring[::-1]
+
+print(rev_string("porcupine"))
+# 'enipucrop'
+
