7cddcbdd853768a5975b2dc8e4cc822a281842fd
Aileen Liu <aileenliu184@gmail.com>
2019-09-30 21:29:45 -0700

    finished two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,42 @@
+# Given an array of integers, return indices of the two numbers such that they 
+# add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may 
+# not use the same element twice.
+
+# Example:
+
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        # bruteForce method, runtime is O(n^2) because of nested loops
+        for i in range(len(nums)):        
+            for j in range(i+1, len(nums)):
+                sum = nums[i] + nums[j]
+                if sum == target:
+                    return [i, j]
+
+        # Linear runtime solution using hashmap
+        # 9 - 2 = 7 -> {7:0}
+        # 9 - 7 = 2 -> {7:0, 2:1}
+        # return nums[i], i
+        
+        temp = dict()
+        
+        for i in range(len(nums)):
+            if nums[i] in temp:
+                return [temp[nums[i]],i]
+            else:
+                temp[target-nums[i]] = i
+                
