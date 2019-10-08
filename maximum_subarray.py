692444a0284b1e990081f2ba2b0dc178c1951c6b
Aileen Liu <aileenliu184@gmail.com>
2019-10-08 16:12:42 -0700

    finished max_subarray.py

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -20,4 +20,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+    if not nums:
+        return 0
+
+    cur_sum = nums[0]
+    max_sum = nums[0]
+
+    for num in nums[1:]:
+        cur_sum = max(num, cur_sum + num)
+        max_sum = max(cur_sum, max_sum)
+
+    return max_sum
